639026f88626cc069fa71b5c11673617577e47f2
Hernan <magoarcano@gmail.com>
2010-10-03 15:47:56 -0300

    Primer commit, rails basico sin generar scaffold ni nada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practico1::Application.config.secret_token = '24438db14ce1a83a7a8f8c05f40f1d069b0ca48108426bfa8fc5bb4ff2674f64b4f951ff2f27b96f3116287e9232e7daeba2a293fd137cf8ec64b425abe901c0'
